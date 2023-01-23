0b5e1c54579c45c6ae0cd5b340d416affd769711
kurabyo <bladvazovski@gmail.com>
2023-01-23 20:59:06 +0200

    💀

--- src/components/ConcentrationArea/ConcentrationArea.jsx
+++ src/components/ConcentrationArea/ConcentrationArea.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Circle } from "@react-google-maps/api";
 
 export function ConcentrationArea({position, concentration}) {
